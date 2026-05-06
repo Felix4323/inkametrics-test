ddb79497fa10e4bc873367ef814bc898267ff82a
Félix Luis López <fl.lopez.321@gmail.com>
2026-05-06 17:45:55 -0500

    codigo para despliegue - properties

--- src/main/java/pe/edu/upc/inkametrics/repositories/ICanalesRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.inkametrics.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.inkametrics.entities.Canales;
-
-@Repository
-public interface ICanalesRepository extends JpaRepository<Canales,Integer> {
-}
